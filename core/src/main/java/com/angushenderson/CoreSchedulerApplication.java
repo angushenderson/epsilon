11a11ca05f9a49512cbee09b892a3f6a7e691a70
Angus Henderson <angushenderson04@gmail.com>
2023-07-10 21:40:37 +0100

    initial commit

---
+++ core/src/main/java/com/angushenderson/CoreSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.angushenderson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreSchedulerApplication.class, args);
+    }
+
+}
