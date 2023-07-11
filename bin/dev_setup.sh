268da0c5a2a6bec3f124efbf9a0f192487734a8d
Angus Henderson <angushenderson04@gmail.com>
2023-07-11 23:12:28 +0100

    containerise rest application and create k8s deployment

--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -1,6 +1,12 @@
+mvn clean install
+eval $(minikube -p minikube docker-env)
+docker build -t dev.angushenderson/epsilon-rest ./epsilon-rest
+#minikube image load dev.angushenderson/epsilon-rest
+
 minikube status
 
-kubectl apply -f ../kube/
+kubectl delete -f ./kube/03-epsilon-rest.yaml --force --grace-period=0
+kubectl apply -f ./kube/
 kubectl config set-context --current --namespace=epsilon
 
 kubectl get all
