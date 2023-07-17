76f9537106f9562f3c2d7eaa1a1e736f3b011cdb
Angus Henderson <angushenderson04@gmail.com>
2023-07-17 11:39:01 +0100

    update dev script to start minikube if not running

--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -1,12 +1,24 @@
+### LOCAL MINIKUBE K8S CLUSTER INITIALIZATION ###
+MINIKUBE_STATUS=$(minikube status | sed -n -e 's/^.*kubelet: //p')
+
+if [[ $MINIKUBE_STATUS != 'Running' ]]
+then
+  minikube start
+fi
+
+
+### BUILD ###
 mvn clean install
 eval $(minikube -p minikube docker-env)
 docker build -t dev.angushenderson/epsilon-rest ./epsilon-rest
-#minikube image load dev.angushenderson/epsilon-rest
 
+
+### DEPLOYMENT INTO MINIKUBE ###
 minikube status
 
-kubectl delete -f ./kube/03-epsilon-rest.yaml --force --grace-period=0
+kubectl delete -f ./kube/03-epsilon-rest.yaml
 kubectl apply -f ./kube/
 kubectl config set-context --current --namespace=epsilon
+#kubectl port-forward deployment/kafka-broker 9092
 
 kubectl get all
